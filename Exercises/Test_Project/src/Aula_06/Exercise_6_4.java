a5821307bbf30d7b3c501d9bf0b3dfb69229b2d9
Gonçalo Silva <goncalo.lslv.silva@gmail.com>
2021-04-23 13:53:46 +0100

    Exercise 6.4 completed

--- Exercises/Test_Project/src/Aula_06/Exercise_6_4.java
+++ Exercises/Test_Project/src/Aula_06/Exercise_6_4.java
@@ -6,7 +6,7 @@ import static java.lang.System.*;
 public class Exercise_6_4 {
 
 	public static void main(String[] args) {
-		if(args.length > 0) {
+		if(args.length < 1) {
 			err.println("Arguments must be provided");
 			exit(1);
 		}
@@ -14,6 +14,10 @@ public class Exercise_6_4 {
 
 	}
 	public static void reverseString(String s) {
+		if(s.length() > 0) {
+			reverseString(s.substring(1));
+			out.print(s.charAt(0));
+		}
 		
 	}
 
