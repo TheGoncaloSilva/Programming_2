b798e2bd9d18da85da1711ee7da4a468079a756b
Gonçalo Silva <goncalo.lslv.silva@gmail.com>
2021-07-05 22:39:38 +0100

    Exercise 10.5 started

---
+++ Exercises/Test_Project/src/Aula_10/Exercise_10_5.java
@@ -0,0 +1,11 @@
+/* For information about the exercise, check the folder class_contents */
+package Aula_10;
+
+public class Exercise_10_5 {
+
+	public static void main(String[] args) {
+		
+
+	}
+
+}
